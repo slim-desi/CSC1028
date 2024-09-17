bcc890ecdff98777217756430cdf5ea4f9751e38
slim-desi <joseph.karippai2@mail.dcu.ie>
2024-09-17 13:58:15 +0100

    Create week2.r

---
+++ Excercises/Week2/week2.r
@@ -0,0 +1,43 @@
+# Q1
+> prod(26:21)/ (26^6)
+
+# Q2
+> choose(60,10) / choose(100, 10)
+
+# Q3
+> choose(5,3) / choose(15,3)
+
+# Q4
+
+# Q4 A
+> 10/50
+
+# Q4 B
+> (40/50)*(10/49) + (10/50)*(9/49)
+
+# Q4 C
+> (10/50)*(9/49)
+
+# Q4 D
+> (40/50)*(10/50) + (10/50)*(10/50)
+
+# Q5
+
+# Q6
+
+# Q7
+> x <- sample(c("H","T"),1000,replace = T)
+> table(x)
+
+# Q8
+> x <- sample(1:6,600,replace = T)
+> x
+> table(x)/600
+> ps <- table(x)/600
+> ps[2] + ps[4] + ps[6]
+
+# Q9
+> x <- sample(c("HT","TT","HH","TH"),100,replace = T)
+> table(x)
+> table(x)/100
+
